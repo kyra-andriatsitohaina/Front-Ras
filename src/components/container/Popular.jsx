1acee345883b6799c42441dbdeca5bb93394fea7
user1 <user1@atmail.com>
2023-11-29 15:40:36 +0300

    five popular ~k10

--- src/components/container/Popular.jsx
+++ src/components/container/Popular.jsx
@@ -1,4 +1,18 @@
+import { useContext } from "react"
+import { ArticleContext } from "../context/Context"
+import { url_api } from "../../api/urlApi"
+
 const Popular = () => {
+    const [data,setData] = useContext(ArticleContext)
+    const favorite = []
+    for(let i = 0 ; i< data.length;i++){favorite.push(data[i].favorite)}
+    favorite.sort()
+    favorite.reverse()
+    favorite.splice(5,favorite.length)
+    const values = []
+    for(let i = 0 ;i<=4;i++){values.push(...data.filter(x=>x.favorite === favorite[i]))}
+    const favoriteValues = [...new Set(values)]
+
     return (
         <section className="popular-properties" id="popular" style={{paddingTop:"12vw"}}>
             <div className="box-popular" >
@@ -6,26 +20,14 @@ const Popular = () => {
                     <h2>popular properties</h2>
                 </div>
                 <div className="popular">
-                    <div className="box-img">
-                        <img src="img/background-house-1.png" alt=""/>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, est.</p>
-                    </div>
-                    <div className="box-img">
-                        <img src="img/background-house-1.png" alt=""/>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, est.</p>
-                    </div>
-                    <div className="box-img">
-                        <img src="img/background-house-1.png" alt=""/>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, est.</p>
-                    </div>
-                    <div className="box-img">
-                        <img src="img/background-house-1.png" alt=""/>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, est.</p>
-                    </div>
-                    <div className="box-img">
-                        <img src="img/background-house-1.png" alt=""/>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, est.</p>
-                    </div>
+                    {
+                        favoriteValues.map((art)=>(
+                            <div className="box-img">
+                                <img src={`${url_api.images}${art.image}`} alt=""/>
+                                <p>{art.description}</p>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </section>
